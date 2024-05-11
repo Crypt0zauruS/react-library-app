4b2faba483639628c5f46dfbd1a6ad57e2efcc13
Crypt0zauruS <101745417+Crypt0zauruS@users.noreply.github.com>
2024-05-11 16:59:41 -0400

    Updated infos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,19 +5,10 @@ const Footer = () => {
     <footer className="footer text-white bg-secondary mt-5">
       <div className="container p-3">
         <p>
-          &copy; Copyright by Crypt0zauruS
+          &copy; Copyright by Crypt0zauruS, {new Date().getFullYear()}
           <span>
             {" "}
             Follow me on{" "}
-            <a
-              className="twitter"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://twitter.com/CryptosaurusRe4"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>{" "}
-            and{" "}
             <a
               className="github"
               target="_blank"
